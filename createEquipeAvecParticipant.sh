16afe07f00246d7e2345206ba2f851a9b173407e
Stephane ISNARD <stephane.isnard@gmail.com>
2014-12-17 19:17:30 +0100

    creation d'une equipe avec un participant

---
+++ createEquipeAvecParticipant.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#éé
+me=`basename $0`
+IFS='.' read -a array <<< "$me"
+fichier="${array[0]}.js" 
+fichierSansExt="${array[0]}"
+
+if [ ! -d datas/$fichierSansExt ]; then
+    mkdir datas/$fichierSansExt
+fi
+if [ ! -d datas/$fichierSansExt/captures ]; then
+    mkdir datas/$fichierSansExt/captures
+else
+    rm datas/$fichierSansExt/captures/capture*
+fi
+cat \
+./src/head.js \
+./lib/createLeader.js \
+./lib/createEquipe.js \
+./lib/createParticipant.js \
+./lib/deleteParticipant.js \
+./lib/deleteEquipe.js \
+./src/capture.js \
+./src/foot.js \
+> $fichier
+
+casperjs test $fichier
