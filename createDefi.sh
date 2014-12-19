4b74aeb918a8f5001b7c199ebd5228e6794b411d
Stephane ISNARD <stephane.isnard@gmail.com>
2014-12-19 14:33:36 +0100

    test creation defi

---
+++ createDefi.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#é
+me=`basename $0`
+IFS='.' read -a array <<< "$me"
+fichier="${array[0]}.js" 
+fichierSansExt="${array[0]}"
+
+if [ ! -d datas/$fichierSansExt ]; then
+    mkdir datas/$fichierSansExt
+fi
+if [ ! -d datas/$fichierSansExt/captures ]; then
+    mkdir datas/$fichierSansExt/captures
+else
+    rm datas/$fichierSansExt/captures/capture*
+fi
+cat \
+./src/head.js \
+./lib/createPartenaire.js \
+./lib/loginPartenaire.js \
+./lib/createDefi.js \
+./src/capture.js \
+./src/logout.js \
+./lib/loginAdmin.js \
+./lib/allerDefi.js \
+./lib/deleteDefi.js \
+./lib/allerPartenaire.js \
+./lib/deletePartenaire.js \
+./src/capture.js \
+./src/foot.js \
+> $fichier
+
+casperjs test $fichier
