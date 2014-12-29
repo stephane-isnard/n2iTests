f53c2a8a98c564467c16c83cddb80fc57ab416d7
Stephane ISNARD <stephane.isnard@gmail.com>
2014-12-29 16:55:36 +0100

    renommage pour mettre un ordre dans lancement

---
+++ 20_createEquipe.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+me=`basename $0`
+IFS='.' read -a array <<< "$me"
+fichier="${array[0]}.js" 
+fichierSansExt="${array[0]}"
+
+if [ ! -d datas/scripts/$fichierSansExt ]; then
+    mkdir datas/scripts/$fichierSansExt
+fi
+if [ ! -d datas/scripts/$fichierSansExt/captures ]; then
+    mkdir datas/scripts/$fichierSansExt/captures
+else
+    rm datas/scripts/$fichierSansExt/captures/capture*
+fi
+cat \
+./src/head.js \
+./lib/createLeader.js \
+./lib/loginLeader.js \
+./lib/createEquipe.js \
+./lib/deleteEquipe.js \
+./src/capture.js \
+./src/foot.js \
+> scripts/$fichier
+
+casperjs test scripts/$fichier
